31e80375e8243588a77d7978c57d56d43a393250
Diriector_Doc <davijust213@yahoo.ca>
2022-05-26 01:50:02 -0400

    Starting slow

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 function $(selector) {
     return document.querySelector(selector);
 }
+class Card {
+    constructor(val, suit) {
+        this._value = val;
+        this._suit = suit;
+    }
+}
 window.onload = () => {
-    $(".overlay").append(jsx `<div class="card" value=2 suit="spades" style="height:112px;width:80px">`);
+    $("#overlay").append(jsx `<div class="card" value=2 suit="spades" style="height:112px;width:80px">`);
 };
--- index.ts
+++ index.ts
@@ -1,9 +1,34 @@
 declare function jsx(str: TemplateStringsArray): HTMLElement;
 
 function $(selector: string){
-    return document.querySelector(selector)
+	return document.querySelector(selector)
+}
+
+const enum Value {
+	ACE,
+	JACK = 11,
+	QUEEN = 12,
+	KING = 13
+}
+
+const enum Suit {
+	SPADES,
+	HEARTS,
+	CLUBS,
+	DIAMONDS
+}
+
+class Card {
+
+	private _value;
+	private _suit;
+
+	constructor(val: Value, suit: Suit){
+		this._value = val;
+		this._suit = suit;
+	}
 }
 
 window.onload = () => {
-    $(".overlay")!.append(jsx`<div class="card" value=2 suit="spades" style="height:112px;width:80px">`)
+	$("#overlay")!.append(jsx`<div class="card" value=2 suit="spades" style="height:112px;width:80px">`)
 }
