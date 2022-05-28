8c7df1faa4ff27b6fff36e2633c13e91b3cdd675
Diriector_Doc <davijust213@yahoo.ca>
2022-05-27 21:19:25 -0400

    Movement working

--- index.js
+++ index.js
@@ -2,12 +2,76 @@
 function $(selector) {
     return document.querySelector(selector);
 }
+const TABLE = jsx `<div id="table">`, HAND = jsx `<div id="hand">`, OVERLAY = jsx `<div id="overlay">`, PLAY_AREA = jsx `<div id="play-area">`;
+PLAY_AREA.append(TABLE, HAND, OVERLAY);
+const ACE = "ace", JACK = "jack", QUEEN = "queen", KING = "king", SPADES = "spades", HEARTS = "hearts", CLUBS = "clubs", DIAMONDS = "diamonds";
 class Card {
     constructor(val, suit) {
         this._value = val;
         this._suit = suit;
+        this.element = jsx `<div class="card" value="${this._value}" suit="${this._suit}">`;
+        this.scale = 1;
+        this.element.onmousedown = (e) => {
+            e.preventDefault();
+            this.x = e.x;
+            this.y = e.y;
+            document.onmousemove = (e) => {
+                e.preventDefault();
+                this.x = e.x;
+                this.y = e.y;
+            };
+            document.onmouseup = (e) => {
+                e.preventDefault();
+                document.onmouseup = document.onmousemove = null;
+                this.x = e.x;
+                this.y = e.y;
+            };
+        };
+    }
+    set scale(val) {
+        /* w = 2.5*32; h = 3.5*32 */
+        this.element.style.width = `${this.width = val * 80}px`;
+        this.element.style.height = `${this.height = val * 112}px`;
+    }
+    set x(px) {
+        this.element.style.left = `${Math.max(0, Math.min((OVERLAY.clientWidth - this.width) / OVERLAY.clientWidth, (px - this.width / 2) / OVERLAY.clientWidth) * 100)}%`;
+    }
+    get x() {
+        return this.element.clientLeft + this.element.clientWidth / 2;
+    }
+    set y(px) {
+        this.element.style.top = `${Math.max(0, Math.min((OVERLAY.clientHeight - this.height) / OVERLAY.clientHeight, (px - this.height / 2) / OVERLAY.clientHeight) * 100)}%`;
+    }
+    get y() {
+        return this.element.clientTop + this.element.clientHeight / 2;
+    }
+    get value() {
+        return this._value;
+    }
+    set value(val) {
+        this.element.attributes.value.value = this._value = val;
+    }
+    get suit() {
+        return this._suit;
+    }
+    set suit(suit) {
+        this.element.attributes.suit.value = this._suit = suit;
+    }
+    get width() {
+        return this.element.clientWidth;
+    }
+    set width(val) {
+        this.element.style.width = `${val}px`;
+    }
+    get height() {
+        return this.element.clientHeight;
+    }
+    set height(val) {
+        this.element.style.height = `${val}px`;
     }
 }
+const c = new Card(ACE, SPADES);
 window.onload = () => {
-    $("#overlay").append(jsx `<div class="card" value=2 suit="spades" style="height:112px;width:80px">`);
+    document.body.append(PLAY_AREA);
+    OVERLAY.append(c.element);
 };
--- index.ts
+++ index.ts
@@ -1,34 +1,107 @@
-declare function jsx(str: TemplateStringsArray): HTMLElement;
+declare function jsx(str: TemplateStringsArray, ...values:any[]): HTMLElement;
 
 function $(selector: string){
 	return document.querySelector(selector)
 }
 
-const enum Value {
-	ACE,
-	JACK = 11,
-	QUEEN = 12,
-	KING = 13
-}
+const TABLE = jsx`<div id="table">`,
+	HAND = jsx`<div id="hand">`,
+	OVERLAY = jsx`<div id="overlay">`,
+	PLAY_AREA = jsx`<div id="play-area">`;
+PLAY_AREA.append(TABLE, HAND, OVERLAY)
 
-const enum Suit {
-	SPADES,
-	HEARTS,
-	CLUBS,
-	DIAMONDS
-}
+const ACE = "ace",
+	JACK = "jack",
+	QUEEN = "queen",
+	KING = "king",
+	SPADES = "spades",
+	HEARTS = "hearts",
+	CLUBS = "clubs",
+	DIAMONDS = "diamonds";
+
+type Value = "ace"|2|3|4|5|6|7|8|9|10|"jack"|"queen"|"king";
+type Suit = "spades"|"hearts"|"clubs"|"diamonds";
 
 class Card {
 
-	private _value;
-	private _suit;
+	private _value: Value;
+	private _suit: Suit;
+
+	readonly element: HTMLElement
 
 	constructor(val: Value, suit: Suit){
 		this._value = val;
 		this._suit = suit;
+		this.element = jsx`<div class="card" value="${this._value}" suit="${this._suit}">`
+		this.scale = 1;
+
+		this.element.onmousedown = (e) => {
+			e.preventDefault()
+			this.x = e.x;
+			this.y = e.y;
+			document.onmousemove = (e) => {
+				e.preventDefault()
+				this.x = e.x;
+				this.y = e.y;
+			}
+			document.onmouseup = (e) => {
+				e.preventDefault()
+				document.onmouseup = document.onmousemove = null;
+				this.x = e.x;
+				this.y = e.y;
+			}
+		}
+	}
+
+	set scale(val: number){
+		/* w = 2.5*32; h = 3.5*32 */
+		this.element.style.width = `${this.width = val*80}px`;
+		this.element.style.height = `${this.height = val*112}px`;
+	}
+
+	set x(px: number){
+		this.element.style.left = `${Math.max(0, Math.min((OVERLAY.clientWidth-this.width)/OVERLAY.clientWidth, (px-this.width/2)/OVERLAY.clientWidth)*100)}%`
+	}
+	get x(){
+		return this.element.clientLeft + this.element.clientWidth/2
+	}
+	set y(px: number){
+		this.element.style.top = `${Math.max(0, Math.min((OVERLAY.clientHeight-this.height)/OVERLAY.clientHeight, (px-this.height/2)/OVERLAY.clientHeight)*100)}%`
+	}
+	get y(){
+		return this.element.clientTop + this.element.clientHeight/2
+	}
+
+	get value(): Value {
+		return this._value
+	}
+	set value(val: Value){
+		(this.element.attributes as any).value.value = this._value = val
+	}
+	get suit(): Suit {
+		return this._suit
+	}
+	set suit(suit: Suit){
+		(this.element.attributes as any).suit.value = this._suit = suit
+	}
+
+	get width(){
+		return this.element.clientWidth
+	}
+	set width(val: number){
+		this.element.style.width = `${val}px`;
+	}
+	get height(){
+		return this.element.clientHeight
+	}
+	set height(val: number){
+		this.element.style.height = `${val}px`;
 	}
 }
 
+const c = new Card(ACE, SPADES);
+
 window.onload = () => {
-	$("#overlay")!.append(jsx`<div class="card" value=2 suit="spades" style="height:112px;width:80px">`)
+	document.body.append(PLAY_AREA)
+	OVERLAY.append(c.element)
 }
